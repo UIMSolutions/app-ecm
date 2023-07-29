3b56c334b1e1508e2687b99e76ca8aad3f507ff8
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-29 07:22:40 +0200

    Update

--- source/apps/ecm/package.d
+++ source/apps/ecm/package.d
@@ -2,8 +2,10 @@ module apps.ecm;
 
 mixin(ImportPhobos!());
 
-// Dub
-public import vibe.d;
+// External
+public {
+  import vibe.d;
+}
 
 // UIM
 public import uim.core;
@@ -26,12 +28,18 @@ public {
 }
 
 static this() {
-  AppRegistry.register("apps.ecm",  
-    App("ecmApp", "apps/ecm")
-      .importTranslations()
-      .addRoutes(
-        Route("", HTTPMethod.GET, IndexPageController),
-        Route("/", HTTPMethod.GET, IndexPageController)
-      )
+  auto myApp = App("ecmApp", "apps/ecm");
+
+  with(myApp) {
+    importTranslations;
+    addControllers([
+      "ecm.index": IndexPageController
+    ]);
+    addRoutes(
+      Route("", HTTPMethod.GET, controller("ecm.index")),
+      Route("/", HTTPMethod.GET, controller("ecm.index"))
     );
+  }
+  
+  AppRegistry.register("apps.ecm", myApp);
 }
